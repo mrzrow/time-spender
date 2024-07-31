b375b3ab123a4e45ca3da7be7ffd5ce676125582
zrow <mrzrow@yandex.ru>
2024-08-01 01:41:36 +0300

    added user model

---
+++ backend/core/models/user.py
@@ -0,0 +1,12 @@
+from backend.core.models.base import Base
+
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from backend.core.models.event import Event
+
+
+class User(Base):
+    tg_id: Mapped[str] = mapped_column(unique=True, nullable=False)
+    events: Mapped[list['Event']] = relationship(back_populates='user')
