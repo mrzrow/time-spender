8fce9dbea5c7abdb2db5593381fe051e77c33c21
zrow <mrzrow@yandex.ru>
2024-08-02 01:31:00 +0300

    updated api url

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,14 +1,15 @@
 import os
 
+from pathlib import Path
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
-
 load_dotenv()
+BASE = Path(__file__).parent.parent
 
 
 class DatabaseSettings(BaseModel):
-    url: str = os.environ.get('DB_PATH')
+    url: str = os.environ.get('DB_PATH').format(BASE)
     echo: bool = bool(os.environ.get('DB_ECHO'))
 
 
